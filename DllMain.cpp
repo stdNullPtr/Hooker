23de076b80bf1b0c93fc4066b77f245d4b2f15ed
Antonio Lyubchev <antonio.lyubchev@estafet.com>
2024-02-29 23:33:12 +0000

    Refactor. Fix logging shellcode params. Add file logging. Add filter to log shellcode

--- DllMain.cpp
+++ DllMain.cpp
@@ -5,114 +5,123 @@
 #include <string>
 #include <sstream>
 #include "Hooker.hpp"
+#include <fstream>
+
+std::ofstream file;
 
 void HandleError(const std::string& msg);
 DWORD WINAPI StartRoutine(LPVOID lpParam);
 
 BOOL WINAPI DllMain(
-    HINSTANCE hinstDLL,  // handle to DLL module
-    DWORD fdwReason,     // reason for calling function
-    LPVOID lpReserved)
+	HINSTANCE hinstDLL,  // handle to DLL module
+	DWORD fdwReason,     // reason for calling function
+	LPVOID lpReserved)
 {
-    switch (fdwReason)
-    {
-    case DLL_PROCESS_ATTACH:
-        CreateThread(NULL, NULL, StartRoutine, hinstDLL, NULL, NULL);
-        break;
-    default: break;
-    }
-    return TRUE;
+	switch (fdwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		CreateThread(NULL, NULL, StartRoutine, hinstDLL, NULL, NULL);
+		break;
+	default: break;
+	}
+	return TRUE;
 }
 
 void HandleError(const std::string& msg)
 {
-    std::ostringstream s;
-    s << msg << XorStr("\nError code: ") << GetLastError();
+	std::ostringstream s;
+	s << msg << XorStr("\nError code: ") << GetLastError();
 
-    if (GetConsoleWindow())
-    {
-        std::cout << s.str();
-    }
-    else
-    {
-        MessageBox(NULL, s.str().c_str(), XorStr("Error"), MB_OK);
-    }
+	if (GetConsoleWindow())
+	{
+		std::cout << s.str();
+	}
+	else
+	{
+		MessageBox(NULL, s.str().c_str(), XorStr("Error"), MB_OK);
+	}
 }
 
 DWORD WINAPI StartRoutine(LPVOID lpParam)
 {
-    //MessageBox(NULL, XorStr("Routine"), XorStr("Work"), MB_OK);
+	//MessageBox(NULL, XorStr("Routine"), XorStr("Work"), MB_OK);
 
-    if (!AllocConsole())
-    {
-        HandleError(XorStr("Failed to attach console!"));
-        // TODO: this is potentially bad since we definitely did not manually map in a perfect way like the Windows loader does, so we can't rely on it to unload
-        // It definitely does not work currently and the bytes are left in memory.
-        FreeLibraryAndExitThread(static_cast<HMODULE>(lpParam), 0);
-    }
-    (void)freopen_s(reinterpret_cast<FILE**>(stdout), XorStr("CONOUT$"), "w", stdout);
-    (void)freopen_s(reinterpret_cast<FILE**>(stderr), XorStr("CONOUT$"), "w", stderr);
-    std::cout << XorStr("Console initialized!\n");
+	if (!AllocConsole())
+	{
+		HandleError(XorStr("Failed to attach console!"));
+		// TODO: this is potentially bad since we definitely did not manually map in a perfect way like the Windows loader does, so we can't rely on it to unload
+		// It definitely does not work currently and the bytes are left in memory.
+		FreeLibraryAndExitThread(static_cast<HMODULE>(lpParam), 0);
+	}
+	(void)freopen_s(reinterpret_cast<FILE**>(stdout), XorStr("CONOUT$"), "w", stdout);
+	(void)freopen_s(reinterpret_cast<FILE**>(stderr), XorStr("CONOUT$"), "w", stderr);
+	std::cout << XorStr("Console initialized!\n");
 
-    bool bHookWsaSend{ false };
+	bool bHookWsaSend{ false };
 
-    const auto* const pWsaSendToFuncAddr{ (uintptr_t*)((uintptr_t)GetModuleHandle("WS2_32.dll") + 0x2E7B0) };
-    const auto* const pLogShellcodeAddr{ (uintptr_t*)Hooker::LogShellcode };
-    const auto* const pWsaSendToRetAddr{ (uintptr_t*)((char*)pWsaSendToFuncAddr + 0xF) };
-    void* const pTrampolineBlock{ VirtualAlloc(nullptr, 0xFF, MEM_COMMIT, PAGE_EXECUTE_READWRITE) };
+	const HMODULE hWs2Dll{ GetModuleHandle("WS2_32.dll") };
+	const FARPROC wsaSendToFuncAddr{ GetProcAddress(hWs2Dll, "WSASendTo") };
 
-    memcpy(&Hooker::WsaSendTo::trampolineShellcode[24], &pLogShellcodeAddr, sizeof(pLogShellcodeAddr));
-    memcpy(&Hooker::WsaSendTo::trampolineShellcode[sizeof(Hooker::WsaSendTo::trampolineShellcode) - 8], &pWsaSendToRetAddr, sizeof(pWsaSendToFuncAddr));
-    memcpy(pTrampolineBlock, Hooker::WsaSendTo::trampolineShellcode, sizeof(Hooker::WsaSendTo::trampolineShellcode));
-    memcpy(&Hooker::WsaSendTo::hookBytes[6], &pTrampolineBlock, sizeof(pTrampolineBlock));
+	const auto* const pWsaSendToFuncAddr{ (uintptr_t*)wsaSendToFuncAddr };
+	const auto* const pLogShellcodeAddr{ (uintptr_t*)Hooker::LogShellcode };
+	const auto* const pWsaSendToRetAddr{ (uintptr_t*)((const char* const)pWsaSendToFuncAddr + 0xF) };
+	void* const pTrampolineBlock{ VirtualAlloc(nullptr, 0xFF, MEM_COMMIT, PAGE_EXECUTE_READWRITE) };
 
-    std::cout << XorStr("Base addr: ") << std::hex << std::uppercase << lpParam << XorStr(" press INS to exit\n");
-    std::cout << XorStr("Log Shellcode addr: ") << std::hex << std::uppercase << pLogShellcodeAddr << '\n';
-    std::cout << XorStr("Trampoline Shellcode addr: ") << std::hex << std::uppercase << pTrampolineBlock << '\n';
-    std::cout << XorStr("WSASend addr: ") << std::hex << std::uppercase << pWsaSendToFuncAddr << '\n';
-    std::cout << XorStr("WSASend ret addr: ") << std::hex << std::uppercase << pWsaSendToRetAddr << '\n';
-    std::cout << "\n";
+	memcpy(&Hooker::WsaSendTo::trampolineShellcode[27], &pLogShellcodeAddr, sizeof pLogShellcodeAddr);
+	memcpy(&Hooker::WsaSendTo::trampolineShellcode[sizeof Hooker::WsaSendTo::trampolineShellcode - 8], &pWsaSendToRetAddr, sizeof pWsaSendToFuncAddr);
+	memcpy(pTrampolineBlock, Hooker::WsaSendTo::trampolineShellcode, sizeof Hooker::WsaSendTo::trampolineShellcode);
+	memcpy(&Hooker::WsaSendTo::hookBytes[6], &pTrampolineBlock, sizeof pTrampolineBlock);
 
-    while (!(GetAsyncKeyState(VK_INSERT) & 0x1))
-    {
-        if (GetAsyncKeyState(VK_F1) & 0x1)
-        {
-            std::cout << (bHookWsaSend ? "Removing hook...\n" : "Placing hook...\n");
+	std::cout << XorStr("Base addr: ") << std::hex << std::uppercase << lpParam << XorStr(" press INS to exit\n");
+	std::cout << XorStr("Log Shellcode addr: ") << std::hex << std::uppercase << pLogShellcodeAddr << '\n';
+	std::cout << XorStr("Trampoline Shellcode addr: ") << std::hex << std::uppercase << pTrampolineBlock << '\n';
+	std::cout << XorStr("WSASendTo() addr: ") << std::hex << std::uppercase << pWsaSendToFuncAddr << '\n';
+	std::cout << XorStr("WSASendTo() ret addr: ") << std::hex << std::uppercase << pWsaSendToRetAddr << '\n';
+	std::cout << "\n";
 
-            bHookWsaSend = !bHookWsaSend;
+	while (!(GetAsyncKeyState(VK_INSERT) & 0x1))
+	{
+		if (GetAsyncKeyState(VK_F1) & 0x1)
+		{
+			std::cout << (bHookWsaSend ? "Removing hook...\n" : "Placing hook...\n");
 
-            if (bHookWsaSend)
-            {
-                Hooker::WsaSendTo::Hook((void*)pWsaSendToFuncAddr);
-            }
-            else
-            {
-                Hooker::WsaSendTo::UnHook((void*)pWsaSendToFuncAddr);
-            }
+			bHookWsaSend = !bHookWsaSend;
 
-            Sleep(100);
-        }
+			if (bHookWsaSend)
+			{
+				file.open("log.txt", std::ios::app);
+				Hooker::WsaSendTo::Hook((void*)pWsaSendToFuncAddr);
+			}
+			else
+			{
+				Hooker::WsaSendTo::UnHook((void*)pWsaSendToFuncAddr);
+				file.close();
+			}
 
-        Sleep(10);
-    }
+			Sleep(100);
+		}
 
-    // TODO Hooker class that will unhook once it goes out of scope, and make its scope tighter around the while loop
-    Hooker::WsaSendTo::UnHook((void*)pWsaSendToFuncAddr);
+		Sleep(10);
+	}
 
-    if (!VirtualFree(pTrampolineBlock, 0, MEM_RELEASE))
-    {
-        HandleError(XorStr("VirtualFree pTrampolineBlock failed!"));
-    }
+	// TODO Hooker class that will unhook once it goes out of scope, and make its scope tighter around the while loop
+	Hooker::WsaSendTo::UnHook((void*)pWsaSendToFuncAddr);
 
-    (void)fclose(stdout);
-    (void)fclose(stderr);
+	if (!VirtualFree(pTrampolineBlock, 0, MEM_RELEASE))
+	{
+		HandleError(XorStr("VirtualFree pTrampolineBlock failed!"));
+	}
 
-    if (!FreeConsole())
-    {
-        HandleError(XorStr("Failed to free console!"));
-    }
+	(void)fclose(stdout);
+	(void)fclose(stderr);
+	file.close();
 
-    // TODO: this is potentially bad since we definitely did not manually map in a perfect way like the Windows loader does, so we can't rely on it to unload
-    // It definitely does not work currently and the bytes are left in memory.
-    FreeLibraryAndExitThread(static_cast<HMODULE>(lpParam), 0);
+	if (!FreeConsole())
+	{
+		HandleError(XorStr("Failed to free console!"));
+	}
+
+	// TODO: this is potentially bad since we definitely did not manually map in a perfect way like the Windows loader does, so we can't rely on it to unload
+	// It definitely does not work currently and the bytes are left in memory.
+	FreeLibraryAndExitThread(static_cast<HMODULE>(lpParam), 0);
 }
--- Hooker.hpp
+++ Hooker.hpp
@@ -1,148 +1,170 @@
 #pragma once
 #include <cstdint>
+#include <fstream>
 
 static_assert(sizeof(uintptr_t) == 8, "Is this a x64 build?");
 
+extern std::ofstream file;
+
 namespace Hooker
 {
-    inline void LogShellcode(const LPWSABUF buf, const DWORD bufferCount)
-    {
-        if (!buf) return;
-        if (!buf->buf) return;
+	inline void LogShellcode(const LPWSABUF buf, const DWORD bufferCount)
+	{
+		if (!buf) return;
 
-        for (DWORD currBuf{ 0 }; currBuf < bufferCount; currBuf++)
-        {
-            printf("[%ld][%lu] ", currBuf, buf->len);
-            for (size_t i{ 0 }; i < buf->len; i++)
-            {
-                printf("%02hhX ", buf->buf[i]);
-            }
-            printf("\n\n");
-        }
-    }
+		for (DWORD currBufIndex{ 0 }; currBufIndex < bufferCount; currBufIndex++)
+		{
+			const LPWSABUF currBuff{ &buf[currBufIndex] };
+			if (!currBuff) continue;
+			if (currBuff->len <= 0) continue;
 
-    namespace WsaSend
-    {
-        inline unsigned char trampolineShellcode[]{
-            0x51,                                           // push rcx
-            0x53,                                           // push rbx
-            0x52,                                           // push rdx
-            0x41, 0x50,                                     // push r8
-            0x48, 0x8B, 0xCA,                               // mov rcx, rdx
-            0x48, 0x81, 0xEC, 0x00, 0x01, 0x00, 0x00,       // sub rsp, 0x100
-            0xFF, 0x15, 0x02, 0x00, 0x00, 0x00, 0xEB, 0x08, // call procedure
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Placeholder for LogShellcode address
-            0x48, 0x81, 0xC4, 0x00, 0x01, 0x00, 0x00,       // add rsp, 0x100
-            0x41, 0x58,                                     // pop r8
-            0x5A,                                           // pop rdx
-            0x5B,                                           // pop rbx
-            0x59,                                           // pop rcx
-            0x48, 0x89, 0x5C, 0x24, 0x08,                   // mov [rsp+0x8], rbx
-            0x48, 0x89, 0x6C, 0x24, 0x10,                   // mov [rsp+0x10], rbp
-            0x48, 0x89, 0x74, 0x24, 0x18,                   // mov [rsp+0x18], rsi
-            0xFF, 0x25, 0x00, 0x00, 0x00, 0x00,             // jmp qword ptr [rip + 0x0]
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00  // Address placeholder for return after hook
-        };
+			if (currBufIndex == 0
+				&& currBuff->len == 2
+				&& (currBuff->buf[0] == 0x00 || currBuff->buf[0] == 0x10 || currBuff->buf[0] == 0x20 || currBuff->buf[0] == 0x30)
+				&& currBuff->buf[1] == 0x00)
+				return; // skip this heartbeat thingy
 
-        inline constexpr unsigned char originalBytes[]{
-            0x48, 0x89, 0x5C, 0x24, 0x08,                   // mov [rsp+08], rbx
-            0x48, 0x89, 0x6C, 0x24, 0x10,                   // mov [rsp+10], rbp
-            0x48, 0x89, 0x74, 0x24, 0x18                    // mov [rsp+18], rsi
-        };
+			if (bufferCount == 2
+				&& currBuff->len == 4)
+				return; // skip the timer? these packets just increment
 
-        inline unsigned char hookBytes[]{
-            0xFF, 0x25, 0x00, 0x00, 0x00, 0x00,             // jmp qword ptr [rip + 0x0]
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Address placeholder
-            0x90                                            // nop
-        };
+			if (currBufIndex < 4) continue;
 
-        static_assert(sizeof(originalBytes) == sizeof(hookBytes), "Hook bytes size differs from original bytes size");
+			printf("[%ld/%ld][%02lu] ", currBufIndex + 1, bufferCount, currBuff->len);
+			for (size_t i{ 0 }; i < currBuff->len; i++)
+			{
+				printf("%02hhX ", currBuff->buf[i]);
+			}
 
-        inline void UnHook(void* const pHookAddress)
-        {
-            DWORD oldProtect;
-            VirtualProtect(pHookAddress, sizeof(originalBytes), PAGE_READWRITE, &oldProtect);
+			printf("\n");
 
-            memcpy(pHookAddress, originalBytes, sizeof originalBytes);
+			file.write(currBuff->buf, currBuff->len);
+		}
+	}
 
-            static DWORD unused;
-            VirtualProtect(pHookAddress, sizeof(originalBytes), oldProtect, &unused);
-        }
+	namespace WsaSend
+	{
+		inline unsigned char trampolineShellcode[]{
+			0x51,                                           // push rcx
+			0x53,                                           // push rbx
+			0x52,                                           // push rdx
+			0x41, 0x50,                                     // push r8
+			0x48, 0x8B, 0xCA,                               // mov rcx, rdx
+			0x48, 0x81, 0xEC, 0x00, 0x01, 0x00, 0x00,       // sub rsp, 0x100
+			0xFF, 0x15, 0x02, 0x00, 0x00, 0x00, 0xEB, 0x08, // call procedure
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Placeholder for LogShellcode address
+			0x48, 0x81, 0xC4, 0x00, 0x01, 0x00, 0x00,       // add rsp, 0x100
+			0x41, 0x58,                                     // pop r8
+			0x5A,                                           // pop rdx
+			0x5B,                                           // pop rbx
+			0x59,                                           // pop rcx
+			0x48, 0x89, 0x5C, 0x24, 0x08,                   // mov [rsp+0x8], rbx
+			0x48, 0x89, 0x6C, 0x24, 0x10,                   // mov [rsp+0x10], rbp
+			0x48, 0x89, 0x74, 0x24, 0x18,                   // mov [rsp+0x18], rsi
+			0xFF, 0x25, 0x00, 0x00, 0x00, 0x00,             // jmp qword ptr [rip + 0x0]
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00  // Address placeholder for return after hook
+		};
 
-        inline void Hook(void* const pHookAddress)
-        {
-            DWORD oldProtect;
-            VirtualProtect(pHookAddress, sizeof(originalBytes), PAGE_READWRITE, &oldProtect);
+		inline constexpr unsigned char originalBytes[]{
+			0x48, 0x89, 0x5C, 0x24, 0x08,                   // mov [rsp+08], rbx
+			0x48, 0x89, 0x6C, 0x24, 0x10,                   // mov [rsp+10], rbp
+			0x48, 0x89, 0x74, 0x24, 0x18                    // mov [rsp+18], rsi
+		};
 
-            memcpy(pHookAddress, hookBytes, sizeof hookBytes);
+		inline unsigned char hookBytes[]{
+			0xFF, 0x25, 0x00, 0x00, 0x00, 0x00,             // jmp qword ptr [rip + 0x0]
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Address placeholder
+			0x90                                            // nop
+		};
 
-            static DWORD unused;
-            VirtualProtect(pHookAddress, sizeof(originalBytes), oldProtect, &unused);
-        }
-    }
+		static_assert(sizeof(originalBytes) == sizeof(hookBytes), "Hook bytes size differs from original bytes size");
 
-    namespace WsaSendTo
-    {
-        inline unsigned char trampolineShellcode[]{
-            0x50,                                           // push rax
-            0x51,                                           // push rcx
-            0x53,                                           // push rbx
-            0x52,                                           // push rdx
-            0x41, 0x50,                                     // push r8
-            0x48, 0x8B, 0xCA,                               // mov rcx, rdx
-            0x48, 0x81, 0xEC, 0x00, 0x01, 0x00, 0x00,       // sub rsp, 0x100
-            0xFF, 0x15, 0x02, 0x00, 0x00, 0x00,             // call qword ptr [rip+0x02]
-            0xEB, 0x08,                                     // jmp short (to next instruction + 0x08)
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Address placeholder
-            0x48, 0x81, 0xC4, 0x00, 0x01, 0x00, 0x00,       // add rsp, 0x100
-            0x41, 0x58,                                     // pop r8
-            0x5A,                                           // pop rdx
-            0x5B,                                           // pop rbx
-            0x59,                                           // pop rcx
-            0x58,                                           // pop rax
-            0x48, 0x8B, 0xC4,                               // mov rax, rsp
-            0x48, 0x89, 0x58, 0x08,                         // mov [rax+0x8], rbx
-            0x48, 0x89, 0x68, 0x10,                         // mov [rax+0x10], rbp
-            0x48, 0x89, 0x70, 0x18,                         // mov [rax+0x18], rsi
-            0xFF, 0x25, 0x00, 0x00, 0x00, 0x00,             // jmp qword ptr [rip+0x00]
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00  // Address placeholder
-        };
+		inline void UnHook(void* const pHookAddress)
+		{
+			DWORD oldProtect;
+			VirtualProtect(pHookAddress, sizeof(originalBytes), PAGE_READWRITE, &oldProtect);
 
-        inline constexpr unsigned char originalBytes[]{
-            0x48, 0x8B, 0xC4,                               // mov rax, rsp
-            0x48, 0x89, 0x58, 0x08,                         // mov [rax+0x8], rbx
-            0x48, 0x89, 0x68, 0x10,                         // mov [rax+0x10], rbp
-            0x48, 0x89, 0x70, 0x18                          // mov [rax+0x18], rsi
-        };
+			memcpy(pHookAddress, originalBytes, sizeof originalBytes);
 
-        inline unsigned char hookBytes[]{
-            0xFF, 0x25, 0x00, 0x00, 0x00, 0x00,             // jmp qword ptr [rip+0x00]
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Address placeholder
-            0x90                                            // nop (No Operation, used for padding/instruction alignment)
-        };
+			static DWORD unused;
+			VirtualProtect(pHookAddress, sizeof(originalBytes), oldProtect, &unused);
+		}
 
-        static_assert(sizeof(originalBytes) == sizeof(hookBytes), "Hook bytes size differs from original bytes size");
+		inline void Hook(void* const pHookAddress)
+		{
+			DWORD oldProtect;
+			VirtualProtect(pHookAddress, sizeof(originalBytes), PAGE_READWRITE, &oldProtect);
 
-        inline void UnHook(void* const pHookAddress)
-        {
-            DWORD oldProtect;
-            VirtualProtect(pHookAddress, sizeof(originalBytes), PAGE_READWRITE, &oldProtect);
+			memcpy(pHookAddress, hookBytes, sizeof hookBytes);
 
-            memcpy(pHookAddress, originalBytes, sizeof originalBytes);
+			static DWORD unused;
+			VirtualProtect(pHookAddress, sizeof(originalBytes), oldProtect, &unused);
+		}
+	}
 
-            static DWORD unused;
-            VirtualProtect(pHookAddress, sizeof(originalBytes), oldProtect, &unused);
-        }
+	namespace WsaSendTo
+	{
+		inline unsigned char trampolineShellcode[]{
+			0x50,                                           // push rax
+			0x51,                                           // push rcx
+			0x53,                                           // push rbx
+			0x52,                                           // push rdx
+			0x41, 0x50,                                     // push r8
+			0x48, 0x8B, 0xCA,                               // mov rcx, rdx     first argument for log func
+			0x49, 0x8B, 0xD0,                               // mov rdx, r8      second argument for log func
+			0x48, 0x81, 0xEC, 0x00, 0x01, 0x00, 0x00,       // sub rsp, 0x100
+			0xFF, 0x15, 0x02, 0x00, 0x00, 0x00,             // call qword ptr [rip+0x02]
+			0xEB, 0x08,                                     // jmp short (to next instruction + 0x08)
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Address placeholder
+			0x48, 0x81, 0xC4, 0x00, 0x01, 0x00, 0x00,       // add rsp, 0x100
+			0x41, 0x58,                                     // pop r8
+			0x5A,                                           // pop rdx
+			0x5B,                                           // pop rbx
+			0x59,                                           // pop rcx
+			0x58,                                           // pop rax
+			0x48, 0x8B, 0xC4,                               // mov rax, rsp
+			0x48, 0x89, 0x58, 0x08,                         // mov [rax+0x8], rbx
+			0x48, 0x89, 0x68, 0x10,                         // mov [rax+0x10], rbp
+			0x48, 0x89, 0x70, 0x18,                         // mov [rax+0x18], rsi
+			0xFF, 0x25, 0x00, 0x00, 0x00, 0x00,             // jmp qword ptr [rip+0x00]
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00  // Address placeholder
+		};
 
-        inline void Hook(void* const pHookAddress)
-        {
-            DWORD oldProtect;
-            VirtualProtect(pHookAddress, sizeof(originalBytes), PAGE_READWRITE, &oldProtect);
+		inline constexpr unsigned char originalBytes[]{
+			0x48, 0x8B, 0xC4,                               // mov rax, rsp
+			0x48, 0x89, 0x58, 0x08,                         // mov [rax+0x8], rbx
+			0x48, 0x89, 0x68, 0x10,                         // mov [rax+0x10], rbp
+			0x48, 0x89, 0x70, 0x18                          // mov [rax+0x18], rsi
+		};
 
-            memcpy(pHookAddress, hookBytes, sizeof hookBytes);
+		inline unsigned char hookBytes[]{
+			0xFF, 0x25, 0x00, 0x00, 0x00, 0x00,             // jmp qword ptr [rip+0x00]
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Address placeholder
+			0x90                                            // nop (No Operation, used for padding/instruction alignment)
+		};
 
-            static DWORD unused;
-            VirtualProtect(pHookAddress, sizeof(originalBytes), oldProtect, &unused);
-        }
-    }
+		static_assert(sizeof originalBytes == sizeof hookBytes, "Hook bytes size differs from original bytes size");
+
+		inline void UnHook(void* const pHookAddress)
+		{
+			DWORD oldProtect;
+			VirtualProtect(pHookAddress, sizeof(originalBytes), PAGE_READWRITE, &oldProtect);
+
+			memcpy(pHookAddress, originalBytes, sizeof originalBytes);
+
+			static DWORD unused;
+			VirtualProtect(pHookAddress, sizeof(originalBytes), oldProtect, &unused);
+		}
+
+		inline void Hook(void* const pHookAddress)
+		{
+			DWORD oldProtect;
+			VirtualProtect(pHookAddress, sizeof(originalBytes), PAGE_READWRITE, &oldProtect);
+
+			memcpy(pHookAddress, hookBytes, sizeof hookBytes);
+
+			static DWORD unused;
+			VirtualProtect(pHookAddress, sizeof(originalBytes), oldProtect, &unused);
+		}
+	}
 }
